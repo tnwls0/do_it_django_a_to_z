0b0b11783bc25989ca34cf5716a6f6dd9576012e
tnwls0 <wjstnwls1010@naver.com>
2022-08-11 15:38:33 +0900

    첨부 파일 있을 경우 포스트 상세 페이지에 다운로드 버튼 출력하기

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import os
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
@@ -13,6 +13,12 @@ class Post(models.Model):
         return f'[{self.pk}]{self.title}'
     def get_absoulte_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_exit(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_exit(self):
+        return self.get_file_name().split('.')[-1]
 
 # Create your models here.
 
