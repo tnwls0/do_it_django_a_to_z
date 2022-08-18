fe59a6df72b2b0cadfd78ab13a9fc7282eab0fdc
tnwls0 <wjstnwls1010@naver.com>
2022-08-18 13:09:17 +0900

    crispy-forms 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'blog',
     'single_pages',
     'django_extensions',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'_media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
